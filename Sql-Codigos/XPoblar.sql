fc7cf7ead1eb2b7b264832b1a59429663a2406ba
Karol <karolestefany29@gmail.com>
2025-04-04 23:58:02 -0500

    correcion cruds

---
+++ Sql-Codigos/XPoblar.sql
@@ -0,0 +1,17 @@
+DELETE FROM AuctionByProduct;
+DELETE FROM BidsByAuctions;
+DELETE FROM Bids;
+DELETE FROM Payments;
+DELETE FROM Penalties;
+DELETE FROM Auctions;
+DELETE FROM Products;
+DELETE FROM Categories;
+DELETE FROM Users;
+DELETE FROM PhoneNumbers;
+DELETE FROM Registered;
+DELETE FROM Addresses;
+DELETE FROM ToAddress;
+DELETE FROM PickUp;
+DELETE FROM TypesDelivery;
+
+COMMIT;
